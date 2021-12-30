db5850f8cd7fe554fea18d327c001578b794d819
k.ikeda <o8011389077@gmail.com>
2021-12-30 15:34:34 +0900

    ファイル名変更による修正

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import Header from '@/components/header.vue'
+import Header from '@/components/Header.vue'
 
 export default Vue.extend({
   name: 'app',
