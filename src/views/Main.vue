baf36a0e14c930d86d86c7e8d74ff915c4a02e49
k.ikeda <o8011389077@gmail.com>
2022-02-06 02:57:04 +0900

    feat:トップ画面のデザインをブラッシュアップ
    一旦はデザインがましになったが、まだ納得していないので、時間ある時に修正したい
    各画面のイメージ画像・説明文の追加

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,20 +1,38 @@
 <template>
-  <div class="Main">
-    <b-card>
-      <b-card-title>
-        <router-link to="/bitcoin">ビットコインレート一覧</router-link>
-      </b-card-title>
-    </b-card>
-    <b-card>
-      <b-card-title>
-        <router-link to="/todo">Todoリスト</router-link>
-      </b-card-title>
-    </b-card>
-    <b-card>
-      <b-card-title>
-        <router-link to="/qiita">Qiitaの記事検索</router-link>
-      </b-card-title>
-    </b-card>
+  <div>
+    <b-card-group deck>
+      <b-card :img-src="require('@/assets/markus-winkler-Q2J2qQsoYH8-unsplash.jpg')">
+        <b-card-title>
+          <router-link to="/todo">Todoリスト</router-link>
+        </b-card-title>
+        <b-card-body>
+          <div>
+            Todo管理を行うことが出来るページです。<br>
+            バックエンドはAWS Lambda(Node.js)です。<br>
+            DBはDynamoDBを使用してます。
+          </div>
+        </b-card-body>
+      </b-card>
+      <b-card :img-src="require('@/assets/pierre-borthiry-vBCVcWUyvyM-unsplash.jpg')">
+        <b-card-title>
+          <router-link to="/bitcoin">ビットコインレート一覧</router-link>
+        </b-card-title>
+        <b-card-body>
+          <div>CoindeskというwebAPIを用いてビットコインレートの一覧を取得するページです。</div>
+        </b-card-body>
+      </b-card>
+      <b-card :img-src="require('@/assets/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.jpg')">
+        <b-card-title>
+          <router-link to="/qiita">Qiitaの記事検索</router-link>
+        </b-card-title>
+        <b-card-body>
+          <div>
+            QiitaAPIというwebAPIを用いて、Qiita内の記事の検索を行うことができるページです。<br>
+            <span class="text-danger">APIに負荷をかけないように、短時間で過度な回数の検索はお控え下さい。</span>
+          </div>
+        </b-card-body>
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 
