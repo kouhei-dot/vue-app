dbb467499dedd68e41cc44fb33fb7eddf9e7b0b6
k.ikeda <o8011389077@gmail.com>
2022-03-14 00:56:49 +0900

    feat:ログイン時のローディングが無かったので追加

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,52 +1,54 @@
 <template>
-  <div class="col-lg-8 m-auto">
-    <b-card
-      class="mt-4"
-      header="ログイン"
-      header-bg-variant="dark"
-      header-text-variant="light"
-      header-class="font-weight-bold"
-    >
-      <b-card-body>
-        <b-alert v-model="isError" fade variant="info">ログインに失敗しました</b-alert>
-        <div class="d-flex flex-row-reverse">
-          <app-action-btn @click="toSignup">新規登録</app-action-btn>
-        </div>
-        <validation-observer v-slot="{ handleSubmit, invalid }">
-          <b-form @submit.prevent="handleSubmit(login)">
-            <validation-provider name="メールアドレス" :rules="{ required: true, email: true }" v-slot="context">
-              <b-form-group label="メールアドレス">
-                <b-form-input type="text" v-model="email" :state="context | validState" />
-                <b-form-invalid-feedback>{{ context.errors[0] }}</b-form-invalid-feedback>
-              </b-form-group>
-            </validation-provider>
-            <validation-provider
-              name="パスワード"
-              :rules="{
-                required: true,
-                regex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])[a-zA-Z\d]*$/,
-                min: 8
-              }"
-              v-slot="context"
-            >
-              <b-form-group label="パスワード">
-                <b-form-input type="password" v-model="password" :state="context | validState" />
-                <b-form-invalid-feedback>{{ context.errors[0] }}</b-form-invalid-feedback>
-              </b-form-group>
-            </validation-provider>
-            <div class="d-flex justify-content-center">
-              <template v-if="isLogin">
-                <app-action-btn @click="$router.push('/top')">トップへ</app-action-btn>
-              </template>
-              <template v-else>
-                <app-action-btn type="submit" :invalid="invalid">ログイン</app-action-btn>
-              </template>
-            </div>
-          </b-form>
-        </validation-observer>
-      </b-card-body>
-    </b-card>
-  </div>
+  <b-overlay class="min-vh-100" :show="isLoading" spinner-type="grow" spinner-variant="info">
+    <div class="col-lg-8 m-auto">
+      <b-card
+        class="mt-4"
+        header="ログイン"
+        header-bg-variant="dark"
+        header-text-variant="light"
+        header-class="font-weight-bold"
+      >
+        <b-card-body>
+          <b-alert v-model="isError" fade variant="info">ログインに失敗しました</b-alert>
+          <div class="d-flex flex-row-reverse">
+            <app-action-btn @click="toSignup">新規登録</app-action-btn>
+          </div>
+          <validation-observer v-slot="{ handleSubmit, invalid }">
+            <b-form @submit.prevent="handleSubmit(login)">
+              <validation-provider name="メールアドレス" :rules="{ required: true, email: true }" v-slot="context">
+                <b-form-group label="メールアドレス">
+                  <b-form-input type="text" v-model="email" :state="context | validState" />
+                  <b-form-invalid-feedback>{{ context.errors[0] }}</b-form-invalid-feedback>
+                </b-form-group>
+              </validation-provider>
+              <validation-provider
+                name="パスワード"
+                :rules="{
+                  required: true,
+                  regex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])[a-zA-Z\d]*$/,
+                  min: 8
+                }"
+                v-slot="context"
+              >
+                <b-form-group label="パスワード">
+                  <b-form-input type="password" v-model="password" :state="context | validState" />
+                  <b-form-invalid-feedback>{{ context.errors[0] }}</b-form-invalid-feedback>
+                </b-form-group>
+              </validation-provider>
+              <div class="d-flex justify-content-center">
+                <template v-if="isLogin">
+                  <app-action-btn @click="$router.push('/top')">トップへ</app-action-btn>
+                </template>
+                <template v-else>
+                  <app-action-btn type="submit" :invalid="invalid">ログイン</app-action-btn>
+                </template>
+              </div>
+            </b-form>
+          </validation-observer>
+        </b-card-body>
+      </b-card>
+    </div>
+  </b-overlay>
 </template>
 
 <script lang="ts">
@@ -61,6 +63,7 @@ export default Vue.extend({
       email: '',
       password: '',
       isError: false,
+      isLoading: false,
     };
   },
   methods: {
@@ -68,6 +71,7 @@ export default Vue.extend({
       await this.$router.push('/signup');
     },
     async login() {
+      this.isLoading = true;
       const auth = getAuth(firebase);
       try {
         const res = await signInWithEmailAndPassword(auth, this.email, this.password);
@@ -80,6 +84,8 @@ export default Vue.extend({
       } catch (e) {
         console.log(e);
         this.isError = true;
+      } finally {
+        this.isLoading = false;
       }
     },
   },
